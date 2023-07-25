7a253fd97d66facd0ea5da84c2ce8fd741d19d6a
Andrey Savelev <sandrig.dev@gmail.com>
2023-07-25 22:34:20 +0300

    refactor: improve algorithm efficiency

--- typescript/src/moveZeroes/moveZeroes.ts
+++ typescript/src/moveZeroes/moveZeroes.ts
@@ -1,17 +1,15 @@
 export function moveZeroes(nums: number[]): void {
   let nonZeroIndex = 0;
 
-  // First, move all non-zero elements to the front of the array
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      nums[nonZeroIndex] = nums[i];
+      if (i !== nonZeroIndex) {
+        // Swap the non-zero element with the element at nonZeroIndex
+        const temp = nums[nonZeroIndex];
+        nums[nonZeroIndex] = nums[i];
+        nums[i] = temp;
+      }
       nonZeroIndex++;
     }
-  }
-
-  // Then, fill the rest of the array with zeroes
-  while (nonZeroIndex < nums.length) {
-    nums[nonZeroIndex] = 0;
-    nonZeroIndex++;
   }
 }
